a8dd213b258b170790660ce2d3af8734b0cbf0bc
Avi Raj <hawkjack0721@gmail.com>
2025-01-07 18:16:35 +0530

    made some changes and added certificates by micro1

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,7 @@ import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.png";
 import project4 from "../assets/projects/project-4.png";
 import certificate1 from "../assets/certificate/Certificate-ICAICR-2023.pdf";
+import certificate2 from "../assets/certificate/certificate_micro1.pdf";
 
 export const DESCRIPTION = `I am a passionate robotics software developer with a knack for crafting robust and scalable computer vision modules. With one year of hands-on experience, I have honed my skills in computer vision technologies like OpenCV and YOLOv7, as well as web development technologies including Django, React, Axios, MySQL, PostgreSQL, and MongoDB. I have successfully developed and deployed projects that utilize facial recognition, object detection, and real-time data processing. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences, while continually expanding my knowledge in advanced robotics and automation systems.`;
 
@@ -28,7 +29,7 @@ export const EDUCATION = [
 
 export const EXPERIENCES = [
   {
-    year: "Sept 2023 - Present",
+    year: "Aug 2023 - Sept 2024",
     role: "Computer Vision Intern",
     company: "UDIP Technologies PVT. LTD.",
     description: `As a Computer Vision Intern at UDIP Technologies Pvt. Ltd. in New Delhi, I have played a crucial role in developing and optimizing computer vision algorithms for various applications. My responsibilities have included training YOLOv7 models using datasets of over 10,000 images, achieving a 95% success rate in object detection through deep learning techniques. I have also contributed to improving computer vision models and validating the performance of smart badge systems in real-world trials, attaining a 63% success rate in object detection. Throughout my internship, I have collaborated with a team to enhance the efficiency and effectiveness of vision systems, maintained comprehensive documentation, and demonstrated strong analytical, problem-solving, and communication skills.`,
@@ -88,5 +89,13 @@ export const CERTIFICATIONS = [
     description:
       "Participated in the Sixth International Conference on Advanced Informatics for Computing Research (ICAICR 2023), where I presented a research paper on the capabilities of smart wearable devices. My research focused on the development and implementation of advanced features such as obstacle and pothole detection, traffic signal recognition to assist elderly people in crossing roads, geofencing, and medical reminders. This work highlights the potential of smart wearables to enhance safety, mobility, and health management, showcasing their multifaceted applications in everyday life.",
     pdf_certificate: certificate1,
+  },
+  {
+    title: "Computer Vision Intern Certificate",
+    company: "micro1",
+    year: "Jan 2025",
+    description:
+      "Successfully passed micro1's rigorous AI Interview and officially certified as a Computer Vision Intern. This certification highlights expertise in the field of computer vision, with a focus on developing intelligent systems that leverage advanced AI and machine learning techniques. As a Computer Vision Intern, the role involves creating and optimizing algorithms for tasks like object detection, image classification, and video analytics. The certification signifies strong problem-solving skills, the ability to work with cutting-edge frameworks such as TensorFlow and PyTorch.",
+    pdf_certificate: certificate2,
   },
 ];
