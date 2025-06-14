e0784941dec592085f6d8b9d6f80f0970dcf8a8a
Hawkjack1729 <hawkjack0721@gmail.com>
2025-06-14 23:08:38 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ const App = () => {
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-indigo-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] dark:bg-neutral-950 dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-        {/* <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div> */}
       </div>
       <div className="container mx-auto px-8">
         <Navbar />
